18195f9cf41491b3dfb1d999646826081e5a84c4
임백산 <turuc@naver.com>
2022-05-17 14:54:00 +0900

    코틀린에서의 함수 특수 문법   - 리턴이 한줄에 결정날때, 바로대입

--- app/src/main/java/kr/nepp/listview_20220517/datas/StudentData.kt
+++ app/src/main/java/kr/nepp/listview_20220517/datas/StudentData.kt
@@ -7,8 +7,6 @@ class StudentData(
 
 //    이 학생의 나이를 계산하는 함수.
 
-    fun getKoreanAge() : Int{
+    fun getKoreanAge()  = 2022 - this.birthYear + 1
 
-        return 2022 - this.birthYear + 1
-    }
 }
